48882385fa3bb721b2616deef6cb590790464ee2
Xabi Ezpeleta <xezpeleta@gmail.com>
2024-02-11 23:39:06 +0100

    Remove venv from script

--- install-scripts/install.sh
+++ install-scripts/install.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 sudo sudo apt-get update -y && sudo apt-get install python3-pip zip -y
-
-if [ ! -d ".venv" ]; then
-	python3 -m venv .venv
-fi
-source .venv/bin/activate
-
 pip3 install -r requirements.txt
 
 #git config --global user.name "Xabi Ezpeleta"
