ba4da3bb7b607a45118e59ef3b64e6f2116adaa8
Xabi Ezpeleta <xezpeleta@gmail.com>
2024-02-11 18:00:12 +0100

    Use venv to install dependencies
    Change the git default config

--- install-scripts/install.sh
+++ install-scripts/install.sh
@@ -1,8 +1,14 @@
+#!/bin/bash
 sudo sudo apt-get update -y && sudo apt-get install python3-pip zip -y
+
+if [ ! -d ".venv" ]; then
+	python3 -m venv .venv
+fi
+source .venv/bin/activate
 
 pip3 install -r requirements.txt
 
-git config --global user.name "Jordi Mas"
-git config --global user.email jmas@softcatala.org
+#git config --global user.name "Xabi Ezpeleta"
+#git config --global user.email xezpleta@gmail.com
 
 bash ./versions.sh
