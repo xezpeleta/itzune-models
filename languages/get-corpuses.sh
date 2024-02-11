40f4e4e96a1db4e7ad2fe49ac6f40d9495fda482
Xabi Ezpeleta <xezpeleta@gmail.com>
2024-02-11 20:46:34 +0100

    Download CCMatrix english-basque

--- languages/get-corpuses.sh
+++ languages/get-corpuses.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-git clone --depth=1 https://github.com/Softcatala/parallel-catalan-corpus corpus-raw
+git clone --depth=1 https://github.com/xezpeleta/parallel-basque-corpus corpus-raw
 cd corpus-raw
 
-declare -a arr=("eng-cat" "deu-cat" "ita-cat" "fra-cat" "spa-cat" "nld-cat" "por-cat" "jpn-cat" "glg-cat" "oci-cat")
+#declare -a arr=("eng-cat" "deu-cat" "ita-cat" "fra-cat" "spa-cat" "nld-cat" "por-cat" "jpn-cat" "glg-cat" "oci-cat")
+declare -a arr=("en-eu")
 
 for dirname in "${arr[@]}"; do
     echo Copying $dirname
